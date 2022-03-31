685d109557295c753aa64e2e3ade8af0663a1254
Nery Galvez <nerygalvezgomez@gmail.com>
2022-03-30 23:28:49 -0600

    Lógica con Middlewares

--- js/app.js
+++ js/app.js
@@ -13,8 +13,12 @@ const preLoadedState = {
 //Agrego el middleware al Store
 const middleware = Redux.applyMiddleware(
   loggerMiddleware,
-  agregarOModificarProductoMiddleware
+  agregarOModificarProductoMiddleware,
+  //Llamo a la función que genera el middleware y le envío el valor inicial de 'indice'
+  //para llevar el control de Id de los productos
+  generadorCodigoProductoBuilder(0)
 );
+
 const store = Redux.createStore(reducer, preLoadedState, middleware);
 
 /**
@@ -22,23 +26,32 @@ const store = Redux.createStore(reducer, preLoadedState, middleware);
  *
  * Este método se va a ejecutar cada vez que cambie el estado
  */
-let latestState;
-
-//'subcribe' retorna una función que permite dessubcribirse del evento
-//es útil cuando queremos cambiar de funcionalidad en nuestra aplicación
-//y así liberamos memoria
-const unSubscribe = store.subscribe(() => {
-  let currentState = store.getState();
-
-  if (currentState != latestState) {
-    latestState = currentState;
-
-    ui.renderForm(currentState.producto); //Si se quiere editar un producto se ponen sus atributos en el formulario
-    ui.renderTable(currentState.productos);
-  }
-});
+store.subscribe(
+  dispatchOnChange(store, (state) => {
+    ui.renderForm(state.producto); //Si se quiere editar un producto se ponen sus atributos en el formulario
+    ui.renderTable(state.productos);
+  })
+);
 
 ui.onFormSubmit = (producto) =>
   store.dispatch(agregarOModificarProducto(producto));
 ui.onEliminarClick = (codigo) => store.dispatch(productoEliminado(codigo));
 ui.onEditarClick = (codigo) => store.dispatch(productoSeleccionado(codigo));
+
+/**
+ * Función para eliminar la varible global let latestState;
+ *
+ * @param {*} store
+ */
+function dispatchOnChange(store, dispatch) {
+  let latestState;
+
+  return function () {
+    let currentState = store.getState();
+
+    if (currentState != latestState) {
+      latestState = currentState;
+      dispatch(currentState); //Se ejecuta el dispatch
+    }
+  };
+}
--- js/store.js
+++ js/store.js
@@ -1,4 +1,14 @@
-let indice = 0;
+/**
+ * Buena práctica tener una variable con el nombre de los action para
+ * estandarizar y evitar errores de cómo se escriben si se usan en distintos lados
+ */
+const actionTypes = {
+  ProductoAgregado: "producto-agregado",
+  ProductoModificado: "producto-modificado",
+  ProductoEliminado: "producto-eliminado",
+  ProductoSeleccionado: "producto-seleccionado",
+  ProductoAgregadoOModificado: "producto-agregado-o-modificado",
+};
 
 /**
  * El reducer es el encargado de devolverme un estado según el action
@@ -8,58 +18,24 @@ let indice = 0;
  * @returns
  */
 const reducer = (state, action) => {
-  if (action.type == "producto-agregado") {
-    indice++; //Sé que se está agregando un producto entonces aumento el contador
-    const codigo = indice;
-    const producto = action.payload;
-    const total = producto.cantidad * producto.precio;
+  switch (action.type) {
+    case actionTypes.ProductoAgregado:
+      return productoAgregadoReducer(state, action);
 
-    //Retorno un objeto con la copia de state y con el arreglo
-    // de 'productos' agregando el nuevo producto
-    return {
-      ...state,
-      productos: [...state.productos, { ...producto, codigo, total }],
-    };
+    case actionTypes.ProductoModificado:
+      return productoModificadoReducer(state, action);
+
+    case actionTypes.ProductoEliminado:
+      return productoEliminadoReducer(state, action);
+
+    //Esto es para cuando quiero editar un producto se llenen los campos del formulario
+    case actionTypes.ProductoSeleccionado:
+      return productoSeleccionadoReducer(state, action);
+
+    default:
+      //Si no conozo el action que está llegando retorno el mismo estado
+      return state;
   }
-
-  if (action.type == "producto-modificado") {
-    const producto = action.payload;
-    const productos = state.productos.slice(); //Hago una copia del arreglo original usando 'slice'
-    const codigo = producto.codigo; //Con el código del producto lo voy a buscar en el arreglo de productos
-    const total = producto.cantidad * producto.precio;
-    const old = productos.find((item) => item.codigo == codigo);
-    const index = productos.indexOf(old); //Obtengo la posición del producto que quiero modificar
-    productos[index] = { ...producto, total }; //Modifico el arreglo en la posición que corresponde con el elemento modificado
-    //Retorno los productos ya modificados
-    return {
-      ...state,
-      productos,
-    };
-  }
-
-  if (action.type == "producto-eliminado") {
-    const codigo = action.payload.codigo;
-    const productos = state.productos.filter((item) => item.codigo != codigo); //Obtengo todos los elementos menos el que tenga el código quiero eliminar
-    return {
-      ...state,
-      productos,
-    };
-  } //Esto es para cuando quiero editar un producto se llenen los campos del formulario
-
-  if (action.type == "producto-seleccionado") {
-    const codigo = action.payload.codigo; //Código del producto que quiero modificar
-
-    return {
-      ...state, //Retorno una copia del estado
-      //Ahora sí uso el otro atributo que definí en mi estado, había usado solo 'productos'
-      //Ahora voy a usar 'producto' en singular
-      //Si no encuentra un producto retorno un objeto vacío
-      producto: state.productos.find((x) => x.codigo == codigo) || {},
-    };
-  }
-
-  //Si no conozo el action que está llegando retorno el mismo estado
-  return state;
 };
 
 /**
@@ -78,29 +54,29 @@ const reducer = (state, action) => {
  * como lo hago en la función definida a continuación
  */
 const productoSeleccionado = (codigo) => ({
-  type: "producto-seleccionado",
+  type: actionTypes.ProductoSeleccionado,
   payload: {
     codigo,
   },
 });
 
 const productoEliminado = (codigo) => ({
-  type: "producto-eliminado",
+  type: actionTypes.ProductoEliminado,
   payload: { codigo },
 });
 
 const productoModificado = (payload) => ({
-  type: "producto-modificado",
+  type: actionTypes.ProductoModificado,
   payload,
 });
 
 const productoAgregado = (payload) => ({
-  type: "producto-agregado",
+  type: actionTypes.ProductoAgregado,
   payload,
 });
 
 const agregarOModificarProducto = (payload) => ({
-  type: "producto-agregado-o-modificado",
+  type: actionTypes.ProductoAgregadoOModificado,
   payload,
 });
 
@@ -154,7 +130,7 @@ const loggerMiddleware = (store) => (next) => (action) => {
 const agregarOModificarProductoMiddleware = (store) => (next) => (action) => {
   //Si no es el action que quiero digo que la aplicación siga con el flujo normal (llamo al next)
   //El action va a ser ejecutado por el reducer
-  if (action.type != "producto-agregado-o-modificado") {
+  if (action.type != actionTypes.ProductoAgregadoOModificado) {
     return next(action);
   }
 
@@ -172,3 +148,85 @@ const agregarOModificarProductoMiddleware = (store) => (next) => (action) => {
   store.dispatch(actionToDispatch);
   return store.dispatch(productoSeleccionado(null)); //Le digo que no quiero seleccionar ninguno, un producto que no existe
 };
+
+function productoSeleccionadoReducer(state, action) {
+  const codigo = action.payload.codigo; //Código del producto que quiero modificar
+
+  return {
+    ...state,
+
+    //Ahora sí uso el otro atributo que definí en mi estado, había usado solo 'productos'
+    //Ahora voy a usar 'producto' en singular
+    //Si no encuentra un producto retorno un objeto vacío
+    producto: state.productos.find((x) => x.codigo == codigo) || {},
+  };
+}
+
+function productoEliminadoReducer(state, action) {
+  const codigo = action.payload.codigo;
+  const productos = state.productos.filter((item) => item.codigo != codigo); //Obtengo todos los elementos menos el que tenga el código quiero eliminar
+  return {
+    ...state,
+    productos,
+  };
+}
+
+function productoModificadoReducer(state, action) {
+  const producto = action.payload;
+  const productos = state.productos.slice(); //Hago una copia del arreglo original usando 'slice'
+  const codigo = producto.codigo; //Con el código del producto lo voy a buscar en el arreglo de productos
+  const total = producto.cantidad * producto.precio;
+  const old = productos.find((item) => item.codigo == codigo);
+  const index = productos.indexOf(old); //Obtengo la posición del producto que quiero modificar
+  productos[index] = { ...producto, total }; //Modifico el arreglo en la posición que corresponde con el elemento modificado
+
+  //Retorno los productos ya modificados
+  return {
+    ...state,
+    productos,
+  };
+}
+
+function productoAgregadoReducer(state, action) {
+  const producto = action.payload;
+  const total = producto.cantidad * producto.precio;
+
+  //Retorno un objeto con la copia de state y con el arreglo
+  // de 'productos' agregando el nuevo producto
+  return {
+    ...state,
+    productos: [...state.productos, { ...producto, total }],
+  };
+}
+
+/**
+ * Función que me genera la función del middleware
+ *
+ * Este middleware es para eliminar la variable global que genera el código del producto 'indice'
+ *
+ * @param {*} lastId
+ * @returns
+ */
+function generadorCodigoProductoBuilder(codigoInicial) {
+  let codigo = codigoInicial;
+
+  return (store) => (next) => (action) => {
+    if (action.type != actionTypes.ProductoAgregado) {
+      return next(action);
+    }
+
+    codigo++;
+
+    //action.payload = { ...action.payload, codigo };
+    //return next(action);
+
+    //La forma correcta de hacer lo de arriba es la siguiente porque con el código de
+    //arriba no estamos copiando correctamente los hijos internos del objeto
+    const actionToDispatch = {
+      ...action,
+      payload: { ...action.payload, codigo },
+    };
+
+    return next(actionToDispatch);
+  };
+}
